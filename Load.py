09407e6694341b63589f02daab737b8077d35641
Mara Rinn <mara.rinn@gmail.com>
2016-09-12 22:27:49 +1000

    Add utf-8 coding note, tidy up structure and whitespace.

--- Load.py
+++ Load.py
@@ -1,24 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from sqlalchemy import create_engine,Table
+from tableloader.tableFunctions import *
+from tableloader.tables import metadataCreator
+import ConfigParser, os
 import warnings
-
 import sys
-
-
-
-
 
 warnings.filterwarnings('ignore', '^Unicode type received non-unicode bind param value')
 
-
 if len(sys.argv)<2:
-    print "Load.py destination"
+    print "Load.py db_engine"
     exit()
-
 
 database=sys.argv[1]
 
-
-import ConfigParser, os
 fileLocation = os.path.dirname(os.path.realpath(__file__))
 inifile=fileLocation+'/sdeloader.cfg'
 config = ConfigParser.ConfigParser()
@@ -26,24 +22,9 @@ config.read(inifile)
 destination=config.get('Database',database)
 sourcePath=config.get('Files','sourcePath')
 
-
-
-
-
-
-from tableloader.tableFunctions import *
-
-
-
 print "connecting to DB"
-
-
 engine = create_engine(destination)
 connection = engine.connect()
-
-
-
-from tableloader.tables import metadataCreator
 
 schema=None
 if database=="postgresschema":
@@ -51,16 +32,10 @@ if database=="postgresschema":
 
 metadata=metadataCreator(schema)
 
-
-
 print "Creating Tables"
-
 metadata.drop_all(engine,checkfirst=True)
 metadata.create_all(engine,checkfirst=True)
-
 print "created"
-
-import tableloader.tableFunctions
 
 blueprints.importyaml(connection,metadata,sourcePath)
 categories.importyaml(connection,metadata,sourcePath)
