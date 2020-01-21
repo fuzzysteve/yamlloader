cf02fdc7f61ae7f4f6b78eddc26876f0e14f1e7d
Steve Anderson <steve@fuzzysteve.co.uk>
2020-01-21 14:23:52 +0100

    dogmaAttributeCategories.py

---
+++ tableloader/tableFunctions/dogmaAttributeCategories.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+reload(sys)
+sys.setdefaultencoding("utf-8")
+from sqlalchemy import Table
+
+from yaml import load,dump
+try:
+	from yaml import CSafeLoader as SafeLoader
+	print "Using CSafeLoader"
+except ImportError:
+	from yaml import SafeLoader
+	print "Using Python SafeLoader"
+
+
+
+def importyaml(connection,metadata,sourcePath,language='en'):
+    print "Importing dogma attribute categories"
+    dgmAttributeCategories = Table('dgmAttributeCategories',metadata)
+    
+    print "opening Yaml"
+        
+    trans = connection.begin()
+    with open(os.path.join(sourcePath,'fsd','dogmaAttributeCategories.yaml'),'r') as yamlstream:
+        print "importing"
+        dogmaAttributeCategories=load(yamlstream,Loader=SafeLoader)
+        print "Yaml Processed into memory"
+        for dogmaAttributeCategoryID in dogmaAttributeCategories:
+          attribute = dogmaAttributeCategories[dogmaAttributeCategoryID]
+          connection.execute(dgmAttributeCategories.insert(),
+                             categoryID=dogmaAttributeCategoryID,
+                             categoryName=attribute['name'],
+                             categoryDescription=attribute['description']
+                )
+    trans.commit()
