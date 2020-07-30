ee3d80a1eb1e18db19f70b33980bd78f471ab728
Jay Blunt <59180931+jayblunt@users.noreply.github.com>
2020-07-30 11:20:10 -0700

    python2 -> python3. use cachecontrol. misc fixes.

--- getmarketgroups-esi.py
+++ getmarketgroups-esi.py
@@ -1,6 +1,9 @@
 import requests
 from sqlalchemy import create_engine,MetaData,Table,Column,INTEGER,FLOAT,VARCHAR,UnicodeText,DECIMAL,Boolean,select,literal_column
-import requests_cache
+#import requests_cache
+import redis
+import cachecontrol
+import cachecontrol.caches.redis_cache
 from requests_futures.sessions import FuturesSession
 import requests_futures
 from concurrent.futures import as_completed
@@ -11,9 +14,9 @@ import sys
 
 def getgroups(grouplist):
     groupfuture=[]
-    print "getitems"
+    print("getitems")
     for groupid in grouplist:
-        if isinstance(groupid,basestring) and groupid.startswith("https"):
+        if isinstance(groupid,str) and groupid.startswith("https"):
             groupfuture.append(session.get(str(groupid)))
         else:
             groupfuture.append(session.get(grouplookupurl.format(groupid)))
@@ -48,7 +51,7 @@ def getgroups(grouplist):
                                 )
         else:
             badlist.append(groupdata.result().url)
-            print groupdata.result().url
+            print(groupdata.result().url)
         pbar.update(1)
     return badlist
 
@@ -57,7 +60,7 @@ def getgroups(grouplist):
 
 
 if len(sys.argv)<2:
-    print "Load.py destination"
+    print("Load.py destination")
     exit()
 
 
@@ -68,12 +71,14 @@ if len(sys.argv)==3:
 else:
     language='en'
 
-import ConfigParser, os
+import configparser, os
 fileLocation = os.path.dirname(os.path.realpath(__file__))
 inifile=fileLocation+'/sdeloader.cfg'
-config = ConfigParser.ConfigParser()
+config = configparser.ConfigParser()
 config.read(inifile)
 destination=config.get('Database',database)
+redis_server=config.get('Redis', 'server')
+redis_db=config.get('Redis', 'db')
 
 schema=None
 if database=="postgresschema":
@@ -111,9 +116,12 @@ groupurl="https://esi.evetech.net/latest/markets/groups/?datasource=tranquility"
 grouplookupurl='https://esi.evetech.net/latest/markets/groups/{}/?datasource=tranquility&language=en-us'
 
 errorcount=0
-requests_cache.install_cache("item_cache",backend='redis',expire_after=35000)
+#requests_cache.install_cache("item_cache",backend='redis',expire_after=35000)
 
-base_session=requests_cache.core.CachedSession(cache_name="item_cache",backend='redis',expire_after=35000)
+#base_session=requests_cache.core.CachedSession(cache_name="item_cache",backend='redis',expire_after=35000)
+redis_connection = redis.Redis(host=redis_server, db=redis_db, retry_on_timeout=True, health_check_interval=30)
+base_session = cachecontrol.CacheControl(requests.session(), cachecontrol.caches.redis_cache.RedisCache(redis_connection))
+
 
 
 lookup=select([invMarketGroups])
@@ -149,13 +157,13 @@ while page<=maxpage:
     groupjson=groups.json()
     maingrouplist=maingrouplist+groupjson
 
-print "Page variable is {}".format(page)
+print("Page variable is {}".format(page))
 
 firstbadlist=getgroups(maingrouplist)
-print "Getting badlist"
+print("Getting badlist")
 secondbadlist=getgroups(firstbadlist)
 
 
 trans.commit()
-print "\n\n"
+print("\n\n")
 
